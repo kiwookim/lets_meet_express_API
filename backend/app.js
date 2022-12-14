37c02cb3ce3c09cf0790ad91d04cb6de315acbe0
Ki Woo Kim <kwedgemusic@gmail.com>
2022-12-14 15:57:21 -0500

    error-handling middleware added for errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
+const { ValidationError } = require("sequelize");
 //checking environment key
 const { environment } = require("./config");
 const isProduction = environment === "production";
@@ -40,5 +41,36 @@ app.use(
 	})
 );
 app.use(routes); // Connect all the routes
+
+//regular error handling
+//catch any requests that don't match any of the routes defined and create a server error
+app.use((_req, _res, next) => {
+	const err = new Error("The requested resource couldn't be found.");
+	err.title = "Resource Not Found";
+	err.errors = ["The requested resource couldn't be found."];
+	err.status = 404;
+	next(err);
+});
+// validation in sequelize error handler
+app.use((err, _req, _res, next) => {
+	// check if error is a Sequelize error:
+	if (err instanceof ValidationError) {
+		err.errors = err.errors.map((e) => e.message);
+		err.title = "Validation error";
+	}
+	next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+	res.status(err.status || 500);
+	console.error(err);
+	res.json({
+		title: err.title || "Server Error",
+		message: err.message,
+		errors: err.errors,
+		stack: isProduction ? null : err.stack,
+	});
+});
 
 module.exports = app;
