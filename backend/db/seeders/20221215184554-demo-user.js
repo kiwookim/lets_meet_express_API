4ef60fa84af5a1e41da8a11688743e13fb5da9dc
Ki Woo Kim <kwedgemusic@gmail.com>
2022-12-15 18:58:59 -0500

    added firstName and lstName column

--- backend/db/seeders/20221215184554-demo-user.js
+++ backend/db/seeders/20221215184554-demo-user.js
@@ -13,16 +13,22 @@ module.exports = {
 			options,
 			[
 				{
+					firstName: "Demo",
+					lastName: "Lition",
 					email: "demo@user.io",
 					username: "Demo-lition",
 					hashedPassword: bcrypt.hashSync("password"),
 				},
 				{
+					firstName: "Fake",
+					lastName: "User",
 					email: "user1@user.io",
 					username: "FakeUser1",
 					hashedPassword: bcrypt.hashSync("password2"),
 				},
 				{
+					firstName: "NotReal",
+					lastName: "Thing",
 					email: "user2@user.io",
 					username: "FakeUser2",
 					hashedPassword: bcrypt.hashSync("password3"),
