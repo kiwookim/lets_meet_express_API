2a65ea01dd72b43d7e2938c7befe0e2f251bde14
Ki Woo Kim <kwedgemusic@gmail.com>
2022-12-15 17:12:53 -0500

    user logout backend endpoint added

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -24,4 +24,11 @@ router.post("/", async (req, res, next) => {
 	});
 });
 
+// backend/routes/api/session.js
+// Log out
+router.delete("/", (_req, res) => {
+	res.clearCookie("token");
+	return res.json({ message: "success" });
+});
+
 module.exports = router;
