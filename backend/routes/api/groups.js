92af7aadd0b9bdfb08b627e72bbdeed99d20cf71
Ki Woo Kim <kwedgemusic@gmail.com>
2022-12-21 12:51:05 -0800

    get-groups by currUser done

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Group, GroupImage, Membership } = require("../../db/models");
-const { requireAuth } = require("../../utils/auth");
+const { restoreUser, requireAuth } = require("../../utils/auth");
 
 router.get("/", async (req, res) => {
 	const payload = [];
@@ -24,37 +24,33 @@ router.get("/", async (req, res) => {
 		group.previewImage = url.url;
 		payload.push(group);
 	}
-	res.json(payload);
+	res.json({ Groups: payload });
 });
 
-router.get("/current", requireAuth, async (req, res, next) => {
-	//getGroup that matchces with User.id
-	//add numMembers
-	//add previewImage
-
-	const currUserGroups = await Group.findBy
+router.get("/current", requireAuth, async (req, res) => {
+	const currUserId = req.user.id;
+	const currUserGroups = await Group.findAll({
+		where: { organizerId: currUserId },
+	});
+	const payload = [];
+	for (let group of currUserGroups) {
+		group = group.toJSON();
+		group.numMembers = await Membership.count({
+			where: {
+				groupId: group.id,
+				status: "member",
+			},
+		});
+		const url = await GroupImage.findOne({
+			where: {
+				groupId: group.id,
+			},
+			attributes: ["url"],
+		});
+		group.previewImage = url.url;
+		payload.push(group);
+	}
+	return res.json({ Groups: payload });
 });
 
 module.exports = router;
-
-// expected format
-// URL: /api/groups/current
-// Get all Groups joined or organized by the Current User
-// {
-//   "Groups":[
-//     {
-//       "id": 1,
-//       "organizerId": 1,
-//       "name": "Evening Tennis on the Water",
-//       "about": "Enjoy rounds of tennis with a tight-nit group of people on the water facing the Brooklyn Bridge. Singles or doubles.",
-//       "type": "In person",
-//       "private": true,
-//       "city": "New York",
-//       "state": "NY",
-//       "createdAt": "2021-11-19 20:39:36",
-//       "updatedAt": "2021-11-19 20:39:36",
-//       "numMembers": 10,
-//       "previewImage": "image url",
-//     }
-//   ]
-// }
