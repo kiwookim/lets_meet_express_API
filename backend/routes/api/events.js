23546efb699fcbdf57b009a56a73a439ff91bd80
Ki Woo Kim <kwedgemusic@gmail.com>
2022-12-23 14:23:48 -0800

    copied from getallevents

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,11 +1,21 @@
 const express = require("express");
-const { Event, Group, Venue, User, Attendance } = require("../../db/models");
+const {
+	Event,
+	Group,
+	Venue,
+	User,
+	Attendance,
+	EventImage,
+} = require("../../db/models");
 const router = express.Router();
-
 
 router.get("/", async (req, res, next) => {
 	const payload = [];
-	const events = await Event.findAll();
+	const events = await Event.findAll({
+		attributes: {
+			exclude: ["description", "capacity", "price", "createdAt", "updatedAt"],
+		},
+	});
 	for (let event of events) {
 		event = event.toJSON();
 		event.numAttending = await Attendance.count({
@@ -14,9 +24,44 @@ router.get("/", async (req, res, next) => {
 				status: "attending",
 			},
 		});
-		console.log(event.numAttending);
+		let url = await EventImage.findAll({
+			where: {
+				eventId: event.id,
+				preview: true,
+			},
+			attributes: ["url"],
+		});
+		const lastPreviewImg = url[url.length - 1];
+		if (url.length) {
+			event.previewImage = lastPreviewImg.url;
+		} else {
+			event.previewImage = "No Preview Image Available";
+		}
+		const associatedGroup = await Group.findOne({
+			where: {
+				id: event.groupId,
+			},
+			attributes: ["id", "name", "city", "state"],
+		});
+		event.Group = associatedGroup;
+		//Venue
+		const associatedVenue = await Venue.findOne({
+			where: {
+				id: event.venueId,
+			},
+			attributes: ["id", "city", "state"],
+		});
+		if (associatedVenue) {
+			event.Venue = associatedVenue;
+		} else {
+			event.Venue = null;
+		}
+
+		payload.push(event);
 	}
-	return res.json(events);
+	return res.json({
+		Events: payload,
+	});
 });
 
 module.exports = router;
